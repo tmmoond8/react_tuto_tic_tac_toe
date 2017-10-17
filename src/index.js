783b8db46d45acac62647249ff2948022d5f4166
Taemin Moon <tmmoond8@gmail.com>
2017-10-17 23:16:01 +0900

    T02
    - Square, Board, Game 컴포넌트 추가
    - 일단 격자 모양 tic tac toe 만들기
    - 클릭하면 X로 변경해주기.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Game from './components/Game';
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
