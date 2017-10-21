62b24232484ef1e17f8cb6ed49b6d6c3385590ae
Taemin Moon <tmmoond8@gmail.com>
2017-10-21 14:19:39 +0900

    T03
    - 플레이어 순서 돌아가며 X, O 입력
    - 승리 확인 체킹
    - 게임 메시지

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+// class가 아닌 function 컴포넌트로 변경
+// class Square extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     render() {
+//         return (
+//             <button className="square" onClick={() =>
+//                 this.props.onClick()}>
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+//
 
-    render() {
-        return (
-            <button className="square" onClick={() =>
-                this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
-
 export default Square;
